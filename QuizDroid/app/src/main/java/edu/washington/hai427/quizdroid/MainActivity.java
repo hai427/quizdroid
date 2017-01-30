c5c36a670ddf67b3709eef801468e1f4cb00ef94
Hai Nguyen <hai427@uw.edu>
2017-01-29 23:02:49 -0800

    added listview and TopicOverviewActivity

---
+++ QuizDroid/app/src/main/java/edu/washington/hai427/quizdroid/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.washington.hai427.quizdroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] topics = new String[]{
+                "Math", "Science", "Marvel Heroes"
+        };
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                topics);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, TopicOverviewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
